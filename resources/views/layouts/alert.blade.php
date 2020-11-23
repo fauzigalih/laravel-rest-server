9b4f213863158d95552e295611e3cb2d828e35e8
fauzigalih <fauzigalihas@gmail.com>
2020-11-23 21:16:43 +0700

    Update alert.blade.php

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -1,20 +1,20 @@
 @if(session('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
+        {!! session('success') !!}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
 @elseif(session('warning'))
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        {{ session('warning') }}
+        {!! session('warning') !!}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
 @elseif(session('danger'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('danger') }}
+        {!! session('danger') !!}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
