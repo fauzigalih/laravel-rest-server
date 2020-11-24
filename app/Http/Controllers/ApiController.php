620a47906248621315b08d9de3b61bb521cdc7ff
fauzigalih <fauzigalihas@gmail.com>
2020-11-24 21:33:25 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,39 +16,54 @@ class ApiController extends Controller
 
     public function generate(Request $request) {
         Api::validateData($request);
+
+        $key = Str::random(20);
         $generate = Api::create([
             'email' => $request->email,
-            'key' => Str::random(20),
+            'key' => $key,
             'action' => implode(',', $request->action)
         ]);
+
         if ($generate) {
             return redirect('api')->with('success', 'Data created successfully! <br>
-                                        Key: '. Str::random(20));
+                                        Key: '. $key);
         }else{
             return redirect('api')->with('danger', 'Data failed to generate!');
         }
     }
 
-    public static function validateKeyApi($request) {
+    public function store(Request $request) 
+    {
         if (!$request->key) {
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'Need key to access api service.',
-            ], 400);
+            return Api::requiredKey();
         }else{
-            $key = Api::where('key', $request->key)->count();
-            if ($key === 0) {
-                return response()->json([
-                    'status' => 'failed',
-                    'message' => 'Key not found.',
-                ], 400);
-            }else{
-                if (Api::where('key', $request->key)->where('action', 'like', '%' . 'read' . '%')->count() === 0) {
+            if (!Api::foundKey($request)) return Api::invalidKey();
+            if (!Api::actionKey($request, 'create')) return Api::blockAction();
+
+            $model = Api::where('key', $request->key)->first();
+            if ($request->name && $request->email && $request->phone) {
+                if (User::where('email', 'like', '%' . $request->email . '%')->count() !== 0) {
                     return response()->json([
-                        'status' => 'failed',
-                        'message' => 'Action not allowed.',
+                        'status' => 'accept & canceled',
+                        'to' => $model->email ?? null,
+                        'message' => 'The email has already been taken.'
                     ], 403);
+                }else{
+                    $modelUserRequest = [
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'phone' => $request->phone
+                    ];
+                    User::create($modelUserRequest);
+                    return response()->json([
+                        'status' => 'success',
+                        'to' => $model->email ?? null,
+                        'message' => 'Data created successfully.',
+                        'data' => $modelUserRequest
+                    ], 200);
                 }
+            }else{
+                return Api::failedAction();
             }
         }
     }
@@ -56,42 +71,125 @@ class ApiController extends Controller
     public function show(Request $request)
     {
         if (!$request->key) {
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'Need key to access api service.',
-            ], 400);
+            return Api::requiredKey();
         }else{
-            $key = Api::where('key', $request->key)->count();
-            if ($key === 0) {
+            if (!Api::foundKey($request)) return Api::invalidKey();
+            if (!Api::actionKey($request, 'read')) return Api::blockAction();
+
+            $model = Api::where('key', $request->key)->first();
+            if ($request->id) {
                 return response()->json([
-                    'status' => 'failed',
-                    'message' => 'Key not found.',
-                ], 400);
+                    'status' => 'success',
+                    'to' => $model->email ?? null,
+                    'message' => 'Get single data user.',
+                    'data' => User::where('id', $request->id)->first()
+                ], 200);
             }else{
-                if (Api::where('key', $request->key)->where('action', 'like', '%' . 'read' . '%')->count() === 0) {
-                    return response()->json([
-                        'status' => 'failed',
-                        'message' => 'Action not allowed.',
-                    ], 403);
-                }
+                return response()->json([
+                    'status' => 'success',
+                    'to' => $model->email ?? null,
+                    'message' => 'Get all data user.',
+                    'data' => User::all()
+                ], 200);
             }
         }
-
-        $model = Api::where('key', $request->key)->first();
-        if ($request->id) {
-            return response()->json([
-                'status' => 'success',
-                'to' => $model->email ?? null,
-                'message' => 'Get single data user.',
-                'data' => User::where('id', $request->id)->first()
-            ], 200);
+    }
+    
+    public function update(Request $request)
+    {
+        if (!($request->key && $request->id)) {
+            return Api::requiredKeyId();
         }else{
-            return response()->json([
-                'status' => 'success',
-                'to' => $model->email ?? null,
-                'message' => 'Get all data user.',
-                'data' => User::all()
-            ], 200);
+            if (!Api::foundKey($request)) return Api::invalidKey();
+            if (!Api::actionKey($request, 'update')) return Api::blockAction();
+
+            $model = Api::where('key', $request->key)->first();
+            $modelUser = User::findOrFail($request->id);
+            $modelUserNow = User::findOrFail($request->id);
+            if ($request->name && $request->email && $request->phone) {
+                if ($modelUser->email !== $request->email) {
+                    if (User::where('email', 'like', '%' . $request->email . '%')->count() !== 0) {
+                        return response()->json([
+                            'status' => 'accept & canceled asas',
+                            'to' => $model->email ?? null,
+                            'message' => 'The email has already been taken.'
+                        ], 403);
+                    }
+                }else{
+                    $modelUserRequest = [
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'phone' => $request->phone
+                    ];
+                    $modelUserNow->update($modelUserRequest);
+                    return response()->json([
+                        'status' => 'success',
+                        'to' => $model->email ?? null,
+                        'message' => 'Data updated successfully.',
+                        'data' => [
+                            'before' => [
+                                'name' => $modelUser->name,
+                                'email' => $modelUser->email,
+                                'phone' => $modelUser->phone
+                            ],
+                            'after' => $modelUserRequest
+                        ]
+                    ], 200);
+                }
+            }else{
+                return Api::failedAction();
+            }
+        }
+    }
+    
+    public function destroy(Request $request)
+    {
+        if (!($request->key && $request->id)) {
+            return Api::requiredKeyId();
+        }else{
+            if (!Api::foundKey($request)) return Api::invalidKey();
+            if (!Api::actionKey($request, 'delete')) return Api::blockAction();
+
+            $model = Api::where('key', $request->key)->first();
+            $modelUser = User::findOrFail($request->id);
+            $modelUserNow = User::findOrFail($request->id);
+
+            if ($modelUserNow->delete()) {
+                
+            }
+            if ($request->name && $request->email && $request->phone) {
+                if ($modelUser->email !== $request->email) {
+                    if (User::where('email', 'like', '%' . $request->email . '%')->count() !== 0) {
+                        return response()->json([
+                            'status' => 'accept & canceled asas',
+                            'to' => $model->email ?? null,
+                            'message' => 'The email has already been taken.'
+                        ], 403);
+                    }
+                }else{
+                    $modelUserRequest = [
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'phone' => $request->phone
+                    ];
+                    $modelUserNow->update($modelUserRequest);
+                    return response()->json([
+                        'status' => 'success',
+                        'to' => $model->email ?? null,
+                        'message' => 'Data updated successfully.',
+                        'data' => [
+                            'before' => [
+                                'name' => $modelUser->name,
+                                'email' => $modelUser->email,
+                                'phone' => $modelUser->phone
+                            ],
+                            'after' => $modelUserRequest
+                        ]
+                    ], 200);
+                }
+            }else{
+                return Api::failedAction();
+            }
         }
     }
 }
