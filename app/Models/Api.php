da58a58f05ec81f465cc11c08fc28da157a92c68
fauzigalih <fauzigalihas@gmail.com>
2020-11-24 21:33:28 +0700

    Update Api.php

--- app/Models/Api.php
+++ app/Models/Api.php
@@ -21,4 +21,47 @@ class Api extends Model
             'action' => 'required'
         ]);
     }
+
+    public static function foundKey($request) {
+        return self::where('key', $request->key)->count() !== 0;
+    }
+
+    public static function actionKey($request, $action) {
+        return self::where('key', $request->key)->where('action', 'like', '%' . $action . '%')->count() !== 0;
+    }
+
+    public static function requiredKey() {
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'Need key to access api service.',
+        ], 400);
+    }
+    
+    public static function requiredKeyId() {
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'Need key and id to access api service.',
+        ], 400);
+    }
+
+    public static function invalidKey() {
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'Key not found.',
+        ], 400);
+    }
+
+    public static function blockAction() {
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'Action not allowed.',
+        ], 403);
+    }
+    
+    public static function failedAction() {
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'The action cannot be continued, please check your data again.',
+        ], 403);
+    }
 }
