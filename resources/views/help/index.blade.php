63674a9aa909acde87e15b413c54b553e63abdb2
fauzigalih <fauzigalihas@gmail.com>
2020-11-25 22:02:56 +0700

    page help

--- resources/views/help/index.blade.php
+++ resources/views/help/index.blade.php
@@ -2,4 +2,5 @@
 @section('title', 'Help')
 @section('content')
   <h1>Help</h1>
+  <p>Contact me on instagram <a href="https://www.instagram.com/fauzigalihajisaputro" target="_blank">here</a></p>
 @endsection
