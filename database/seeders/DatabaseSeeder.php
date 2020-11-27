3a2182a89dcd58b4d34e6895b1a88636d454ce1a
fauzigalih <fauzigalihas@gmail.com>
2020-11-27 16:22:30 +0700

    create dummy database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Api;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::insert([
+            'name' => 'Fauzi Galih',
+            'email' => 'fauzi@sample.com',
+            'phone' => 1234567890,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        Api::insert([
+            'email' => 'fauzi@sample.com',
+            'key' => 'AU3t3wWxeR7pJgUKQZFH',
+            'action' => 'create,read,update,delete',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
     }
 }
